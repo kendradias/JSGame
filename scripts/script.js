1f1e3e4cc45fd57e21e223248a208cc26ac5fe34
Kendra Dias <kendra.dias@hotmail.com>
2024-11-12 15:19:37 -0800

    added styling to get the game-board looking how I want

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,58 @@
 'use strict';
 
-console.log("hi");
+const game = {
+    isRunning: false,
+    wasRunning: false,
+    isPaused: false,
+    currentScreen: 'welcome-screen',
+    $progressBar: $('#progress-bar'),
+    $timeDisplay: $('#time-display'),
+    
+
+  //switch screen function
+  switchScreen: function(screen) {
+    //update current screen property
+    this.currentScreen = screen;
+    //hide all screens and show selected screen
+    $('.screen').hide();
+    $(`#${screen}`).show();
+    //if current screen is game over screen, stop game
+    if (this.currentScreen === 'game-over-screen') {
+        this.isRunning = false;
+    }
+  },
+
+  toggleRunning: function () {
+    this.isRunning = !this.isRunning;
+    //update button text to display play/pause 
+    $('#play-pause-btn').text(this.isRunning ? 'Pause' : 'Play');
+    }
+}
+
+//Event Listeners
+$(document).ready(function() {
+    game.switchScreen('welcome-screen');
+
+    //WELCOME SCREEN BUTTONS
+    //go to instructions screen
+    $('#instructions-btn').click(() => game.switchScreen('instructions-screen'));
+
+    //INSTRUCTIONS SCREEN BUTTONS
+    //start game and navigate to game screen on click 
+    $('#play-game-btn').click(() => game.switchScreen('game-screen'));
+
+    //GAME SCREEN BUTTONS
+    //toggle game state on click
+    $('#play-pause-btn').click(() => game.toggleRunning());
+    //end game and navigate to game over screen on click
+    $('#end-game-btn').click(() => game.switchScreen('game-over-screen'));
+    //redirect back to welcome screen on click
+    $('#quit-btn').click(() => game.switchScreen('welcome-screen'));
+
+    //GAME OVER BUTTONS
+    // switch to game screen on click
+    $('#play-again-btn').click(() => game.switchScreen('game-screen'));
+    //redirect back to splash screen on click
+    $('#quit-btn').click(() => game.switchScreen('welcome-screen'));
+    
+});
