fb3919d5467db1e3bda50395a4b657013de20f19
Kendra Dias <kendra.dias@hotmail.com>
2024-12-27 16:37:15 -0800

    completed majority of game logic. animation of player and boundaries and pellets, collision detection for both player vs boundary and player vs pellet, and a score element that updates based on player/pellet collision

--- scripts/script.js
+++ scripts/script.js
@@ -1,95 +1,5 @@
 'use strict';
 
-// canvas reference
-const canvas = document.querySelector('canvas');
-const c = canvas.getContext('2d');
-//ensure canvas is width and height of gameboard
-const gameBoard = document.getElementById('game-board');
-canvas.width = gameBoard.offsetWidth;
-canvas.height = gameBoard.offsetHeight;
-
-//boundary class
-class Boundary {
-    static width = 40;
-    static height = 40;
-    constructor({position}) {
-        this.position = position;
-        this.width = 40;
-        this.height = 40;
-    }
-    draw() {
-        c.fillStyle = 'blue'
-        c.fillRect(this.position.x, this.position.y, 
-            this.width, this.height)
-    }
-}
-//player class
-class Player {
-    constructor({position, velocity}) {
-        this.position = position
-        this.velocity = velocity
-        this.radius = 15
-    }
-    draw() {
-        c.beginPath()
-        c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2)
-        c.fillStyle = 'yellow'
-        c.fill()
-        c.closePath()
-    }
-    update() {
-        this.draw()
-        this.position.x += this.velocity.x
-        this.position.y += this.velocity.y
-    }
-}
-// MAP object 
-const map = [
-    ['-','-','-','-','-','-'],
-    ['-',' ',' ',' ',' ','-'],
-    ['-',' ','-','-',' ','-'],
-    ['-',' ',' ',' ',' ','-'],
-    ['-','-','-','-','-','-']
-]
-// create boundaries as an array 
-const boundaries = []
-
-// instantiate new player
-const player = new Player({
-    position: {
-        x: Boundary.width + Boundary.width / 2,
-        y: Boundary.height + Boundary.height / 2
-    },
-    velocity: {
-        x:0,
-        y:0
-    }
-});
-
-map.forEach((row, i) => {
-    row.forEach((symbol, j) => {
-        switch (symbol) {
-            case '-':
-                boundaries.push(
-                    new Boundary({
-                        position: {
-                            x: Boundary.width * j,
-                            y: Boundary.width * i
-                        }
-                    })
-                )
-                break;
-        }
-    })
-})
-
-// draw boundaries
-boundaries.forEach((boundary) => {
-    boundary.draw();
-})
-//draw player
-player.draw()
-
 // Game object 
 const game = {
     isRunning: false,
@@ -120,25 +30,325 @@ const game = {
     }
 }
 
+// canvas reference
+const canvas = document.querySelector('canvas');
+const c = canvas.getContext('2d');
+
+//score 
+document.addEventListener('DOMContentLoaded', () => {
+    const scoreEl = document.querySelector('#scoreEl');
+    console.log(scoreEl); // Should not be null if the element exists
+});
+
+//ensure canvas is width and height of gameboard
+const gameBoard = document.getElementById('game-board');
+canvas.width = gameBoard.offsetWidth;
+canvas.height = gameBoard.offsetHeight;
+
+
+//boundary class
+class Boundary {
+    static width = 40
+    static height = 40
+    constructor({position}) {
+        this.position = position;
+        this.width = 40
+        this.height = 40
+    }
+    draw() {
+        c.fillStyle = 'blue'
+        c.fillRect(this.position.x, this.position.y, 
+            this.width, this.height)
+
+    }
+}
+
+//player class
+class Player {
+    constructor({position, velocity}) {
+        this.position = position
+        this.velocity = velocity
+        this.radius = 15
+    }
+    draw() {
+        c.beginPath()
+        c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2)
+        c.fillStyle = 'yellow'
+        c.fill()
+        c.closePath()
+    }
+    update() {
+        this.draw()
+        this.position.x += this.velocity.x
+        this.position.y += this.velocity.y
+    }
+}
+
+//Pellet class
+class Pellet {
+    constructor({position, velocity}) {
+        this.position = position
+        this.radius = 3
+    }
+    draw() {
+        c.beginPath()
+        c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2)
+        c.fillStyle = 'white'
+        c.fill()
+        c.closePath()
+    }
+}
+
+// Keys Object 
+const keys = {
+    ArrowUp: {
+        pressed: false
+    }, 
+    ArrowLeft: {
+        pressed: false
+    }, 
+    ArrowRight: {
+        pressed: false
+    }, 
+    ArrowDown: {
+        pressed: false
+    }, 
+}
+// last key to update when a new key is pressed
+let lastKey = '';
+//score property that updates 
+let score = 0
+
+// MAP Object 
+const map = [
+    ['-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-'],
+    ['-',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','-'],
+    ['-',' ','-',' ','-','-','-',' ','-',' ',' ','-',' ','-','-','-',' ','-',' ','-'],
+    ['-',' ',' ',' ',' ','-',' ',' ',' ',' ',' ',' ',' ',' ','-',' ',' ',' ',' ','-'],
+    ['-',' ','-','-',' ',' ',' ','-',' ',' ',' ',' ','-',' ',' ',' ','-','-',' ','-'],
+    ['-',' ','-','-',' ',' ',' ','-','-','-','-','-','-',' ',' ',' ','-','-',' ','-'],
+    ['-',' ',' ',' ',' ','-',' ',' ',' ',' ',' ',' ',' ',' ','-',' ',' ',' ',' ','-'],
+    ['-',' ','-',' ','-','-','-',' ','-',' ',' ','-',' ','-','-','-',' ','-',' ','-'],
+    ['-',' ',' ',' ',' ','-',' ',' ',' ',' ',' ',' ',' ',' ','-',' ',' ',' ',' ','-'],
+    ['-',' ','-','-',' ',' ',' ','-','-','-','-','-','-',' ',' ',' ','-','-',' ','-'],
+    ['-',' ','-','-',' ',' ',' ','-',' ',' ',' ',' ','-',' ',' ',' ','-','-',' ','-'],
+    ['-',' ',' ',' ',' ','-',' ',' ',' ',' ',' ',' ',' ',' ','-',' ',' ',' ',' ','-'],
+    ['-',' ','-',' ','-','-','-',' ','-',' ',' ','-',' ','-','-','-',' ','-',' ','-'],
+    ['-',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','-'],
+    ['-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-'],
+]
+
+// create boundaries as an array 
+const boundaries = []
+//create pellets as an array
+const pellets = []
+//instantiate new player
+const player = new Player({
+    position: {
+        x: Boundary.width + Boundary.width / 2,
+        y: Boundary.height + Boundary.height / 2
+    },
+    velocity: {
+        x:0,
+        y:0
+    }
+});
+
+map.forEach((row, i) => {
+    row.forEach((symbol, j) => {
+        switch (symbol) {
+            case '-':
+                boundaries.push(
+                    new Boundary({
+                        position: {
+                            x: Boundary.width * j,
+                            y: Boundary.width * i
+                        }
+                    })
+                )
+                break;
+            case ' ':
+                pellets.push(
+                    new Pellet({
+                        position: {
+                            x: j * Boundary.width + Boundary.width / 2,
+                            y: i * Boundary.height + Boundary.height / 2
+                        }
+                    })
+                )
+        }
+    })
+})
+
+//collision detection for player vs boundary
+function circleCollidesWithRectangle({circle, rectangle}) {
+    return(circle.position.y - circle.radius + circle.velocity.y <= rectangle.position.y + rectangle.height //if top of circle collides with bottom of a rectangle
+        && circle.position.x + circle.radius + circle.velocity.x >= rectangle.position.x // if right of a circle collides with left of side of a rectangle
+        && circle.position.y + circle.radius + circle.velocity.y >= rectangle.position.y // if bottom of a circle collides with top rectangle
+        && circle.position.x - circle.radius + circle.velocity.x <= rectangle.position.x + rectangle.width)//if left of a circle is collides with right side of a boundary
+}
+
+//player movement animation
+function animate() {
+    requestAnimationFrame(animate)
+    c.clearRect(0, 0, canvas.width, canvas.height)
+
+    if (keys.ArrowUp.pressed && lastKey === 'ArrowUp') {
+        //loop through all boundaries and test collision
+        for (let i = 0; i < boundaries.length; i ++) {
+            const boundary = boundaries[i]
+            if (
+                circleCollidesWithRectangle({
+                    //duplicate player object and reference velocity property
+                    circle: {...player,  velocity: {
+                        x: 0,
+                        y: -5
+                    }
+                },
+                    rectangle: boundary
+                })
+            ) {
+                player.velocity.y = 0
+                break
+            } else {
+                player.velocity.y = -5
+            }
+        }
+    } else if (keys.ArrowLeft.pressed && lastKey === 'ArrowLeft') {
+        for (let i = 0; i < boundaries.length; i ++) {
+            const boundary = boundaries[i]
+            if (
+                circleCollidesWithRectangle({
+                    //duplicate player object and reference velocity property
+                    circle: {...player,  velocity: {
+                        x: -5,
+                        y: 0
+                    }
+                },
+                    rectangle: boundary
+                })
+            ) {
+                player.velocity.x = 0
+                break
+            } else {
+                player.velocity.x = -5
+            }
+        }
+    } else if (keys.ArrowDown.pressed && lastKey === 'ArrowDown') {
+        for (let i = 0; i < boundaries.length; i ++) {
+            const boundary = boundaries[i]
+            if (
+                circleCollidesWithRectangle({
+                    //duplicate player object and reference velocity property
+                    circle: {...player,  velocity: {
+                        x: 0,
+                        y: 5
+                    }
+                },
+                    rectangle: boundary
+                })
+            ) {
+                player.velocity.y = 0
+                break
+            } else {
+                player.velocity.y = 5
+            }
+        }
+    } else if (keys.ArrowRight.pressed && lastKey === 'ArrowRight') {
+        for (let i = 0; i < boundaries.length; i ++) {
+            const boundary = boundaries[i]
+            if (
+                circleCollidesWithRectangle({
+                    //duplicate player object and reference velocity property
+                    circle: {...player,  velocity: {
+                        x: 5,
+                        y: 0
+                    }
+                },
+                    rectangle: boundary
+                })
+            ) {
+                player.velocity.x = 0
+                break
+            } else {
+                player.velocity.x = 5
+            }
+        }
+    }
+
+    // touch pellets here
+    for (let i = pellets.length - 1; 0 < i; i--) {
+        const pellet = pellets[i]
+        pellet.draw()
+        //circle to circle collision detection for player vs pellet
+        if (
+            Math.hypot(
+                pellet.position.x - player.position.x,
+                pellet.position.y - player.position.y
+            ) < pellet.radius + player.radius
+        ) {
+            pellets.splice(i, 1)
+            score += 1
+            scoreEl.innerHTML =score
+        }
+    }
+
+    boundaries.forEach((boundary) => {
+        boundary.draw()
+
+        if (circleCollidesWithRectangle({
+            circle: player, 
+            rectangle: boundary
+        })
+    ) {
+        player.velocity.x = 0
+        player.velocity.y = 0
+    }
+    })
+    player.update();
+}
+animate()
+
+
 //Event Listeners
 //Player Movement Key event Listeners
 addEventListener('keydown', ({key}) => {
     switch (key) {
         case 'ArrowUp':
-            player.velocity.y = -5
+            keys.ArrowUp.pressed = true
+            lastKey = 'ArrowUp'
             break
         case 'ArrowLeft':
-            player.velocity.x = -5
+            keys.ArrowLeft.pressed = true
+            lastKey = 'ArrowLeft'
             break
         case 'ArrowDown':
-            player.velocity.y = 5
+            keys.ArrowDown.pressed = true
+            lastKey = 'ArrowDown'
             break
         case 'ArrowRight':
-            player.velocity.x = 5
+            keys.ArrowRight.pressed = true
+            lastKey = 'ArrowRight'
             break
     }
 })
 
+addEventListener('keyup', ({key}) => {
+    switch (key) {
+        case 'ArrowUp':
+            keys.ArrowUp.pressed = false
+            break
+        case 'ArrowLeft':
+            keys.ArrowLeft.pressed = false
+            break
+        case 'ArrowDown':
+            keys.ArrowDown.pressed = false
+            break
+        case 'ArrowRight':
+            keys.ArrowRight.pressed = false
+            break
+    }
+})
 
 
 $(document).ready(function() {
